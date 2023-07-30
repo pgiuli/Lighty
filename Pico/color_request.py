75eabfa19ea75ab9f40aacbd8694b4379af2421c
Pau Giuli <pau@giuli.cat>
2023-07-30 13:25:17 +0200

    Fixed response not being garbage collected

--- Pico/color_request.py
+++ Pico/color_request.py
@@ -11,7 +11,8 @@ def get_color():
     print('Sending HTTP request to: {}'.format(url))
     response = urequests.get(url)
     data = response.json()
- 
+    response.close() #Garbage collection
+    response = None #Garbage collection
     return data['red'], data['green'], data['blue'], #data['force_update']
 
 
