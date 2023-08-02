460a4db517810f28b4f82397fd229e4de8ffd32c
Pau Giuli <pau@giuli.cat>
2023-08-03 00:59:33 +0200

    Made API values easier to change

--- Pico/color_request.py
+++ Pico/color_request.py
@@ -1,21 +1,23 @@
 #This file manages all requests sent to the API
-
 import urequests
 
 print('Loaded Request File!')
 
-baseurl = 'http://10.1.1.47:8000/lighty-rgb/'
+api_ip = '10.1.1.47'
+api_port = '8000'
+
+apiurl = "http://{}:{}/lighty-rgb/".format(api_ip, api_port)
 
 append = '?hai=true'
 
 def get_color(hai=False):
     
     if hai:
-        requesturl = baseurl + append   
+        requesturl = apiurl + append   
         print('Sending request with hai!')
         #print('Sending HTTP request to: {}'.format(requesturl))
     else:
-        requesturl = baseurl
+        requesturl = apiurl
         #print('Sending HTTP request to: {}'.format(requesturl))
 
     response = urequests.get(requesturl)
